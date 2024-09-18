e83174bd6bb328f7a5f8e40a0cb497fb74f335f8
Magherbi Mohamed Ali <magherbymohamedali@gmail.com>
2024-09-18 12:43:32 +0100

    Update CorsConfig.java

--- src/main/java/tn/biramgroup/pointage/CorsConfig.java
+++ src/main/java/tn/biramgroup/pointage/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://clocking-in-angular-3aee.vercel.app/")
+                        .allowedOrigins("https://biramgroup.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
